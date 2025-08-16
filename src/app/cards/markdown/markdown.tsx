7ca1ebc7e21375886f49fa668db1d275639db0d9
Nausicaa <nlusano@meditools.nl>
2025-08-16 18:24:33 +0200

    Move file

--- src/app/cards/markdown/markdown.tsx
+++ src/app/cards/markdown/markdown.tsx
@@ -1,15 +1,16 @@
 import { Card, CardContent, CardHeader } from "@/components/ui/card";
-import markdown from "@/components/json/markdown.json";
+import { markdown } from "@/components/data/markdown";
 
 export default function MarkdownCard() {
   const { content, title } = markdown;
+  const { details } = content;
   return (
     <Card>
       <CardHeader>
         <h3 className="mb-2 text-xl font-semibold">{title}</h3>
       </CardHeader>
       <CardContent className="p-2 rounded-md text-slate-600 bg-gray-300">
-        <code>{JSON.parse(JSON.stringify(content))}</code>
+        <code>{JSON.parse(JSON.stringify(details))}</code>
       </CardContent>
     </Card>
   );
