f78589b47b4a906705602d6394fb26757a175089
Nausicaa <nlusano@meditools.nl>
2025-08-16 19:13:11 +0200

    Fix display of markdown data snippet, add snippet title

--- src/app/cards/markdown_togglableDetails.tsx
+++ src/app/cards/markdown_togglableDetails.tsx
@@ -2,6 +2,13 @@ import { markdown } from "@/components/data/markdown";
 
 export default function MarkdownTogglableDetails() {
   const { content } = markdown;
-  const { details } = content;
-  return <code>{JSON.parse(JSON.stringify(details))}</code>;
+  const { toggleDetails } = content;
+  return (
+    <article>
+      <div className="rounded-lg pl-2 p-1 mb-2 font-semibold text-md bg-slate-400 text-slate-700 w-auto">
+        {toggleDetails.title.toUpperCase()}
+      </div>
+      <pre>{JSON.parse(JSON.stringify(toggleDetails.snippet))}</pre>
+    </article>
+  );
 }
