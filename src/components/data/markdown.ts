761d2c81c71fbb5c32099823741d00ebfe27c275
Nausicaa <nlusano@meditools.nl>
2025-08-16 18:30:41 +0200

    Readd markdown snippets data as const

---
+++ src/components/data/markdown.ts
@@ -0,0 +1,8 @@
+export const markdown = {
+  title: "Markdown helper",
+  content: {
+    details:
+      '```md\n<details open>\n  <summary>Hello</summary>\n  World!\n  <img src="" />\n\n  <table>\n    <tr>\n      <th>In development:</th>\n      <td><img src="" /></td>\n    </tr>\n    <tr>\n      <th>In this branch:</th>\n      <td><img src="" /></td>\n    </tr>\n  </table>\n</details>\n```',
+  },
+  labels: ["markdown", ".md"],
+};
