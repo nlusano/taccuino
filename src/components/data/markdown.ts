2193834cfb789bac0ce31e259bed0282b7849ff5
Nausicaa <nlusano@meditools.nl>
2025-08-16 19:12:50 +0200

    Fix markdown data snippet, add snippet title

--- src/components/data/markdown.ts
+++ src/components/data/markdown.ts
@@ -1,8 +1,25 @@
 export const markdown = {
   title: "Markdown helper",
   content: {
-    details:
-      '```md\n<details open>\n  <summary>Hello</summary>\n  World!\n  <img src="" />\n\n  <table>\n    <tr>\n      <th>In development:</th>\n      <td><img src="" /></td>\n    </tr>\n    <tr>\n      <th>In this branch:</th>\n      <td><img src="" /></td>\n    </tr>\n  </table>\n</details>\n```',
+    toggleDetails: {
+      title: "Togglable details",
+      snippet: `<details open>
+  <summary>Hello</summary>
+  World!
+  <img src="" />
+
+  <table>
+    <tr>
+      <th>In dev:</th>
+      <td><img src="" /></td>
+    </tr>
+    <tr>
+      <th>In this branch:</th>
+      <td><img src="" /></td>
+    </tr>
+  </table>
+</details>`,
+    },
   },
   labels: ["markdown", ".md"],
 };
