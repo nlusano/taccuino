b0a04c5d35fd168ae27b906b7675cd429ed1837e
Nausicaa <nlusano89@gmail.com>
2025-08-24 17:28:15 +0200

    Fix build err

--- src/components/ui/card.tsx
+++ src/components/ui/card.tsx
@@ -2,7 +2,7 @@ import * as React from "react";
 
 import { cn } from "@/lib/utils";
 
-function CodeSnippet({ className, ...props }: React.ComponentProps<"pre"> & { code: any }) {
+function CodeSnippet({ className, ...props }: React.ComponentProps<"pre"> & { code: string }) {
   return (
     <pre
       data-testid="code-snippet"
