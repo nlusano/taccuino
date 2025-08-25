7262b44bc158e754a4e8c1970519d18b44a1a918
Nausicaa <nlusano89@gmail.com>
2025-08-25 20:03:53 +0200

    Change all section tags to divs in the homepage

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -3,20 +3,20 @@ import MarkdownCard from "./cards/markdown";
 export default function Home() {
   return (
     <main className="min-h-screen bg-slate-300 pt-20 text-slate-900">
-      <section className="mx-auto flex max-w-4xl flex-col gap-12 px-6">
+      <div className="mx-auto flex max-w-4xl flex-col gap-12 px-6">
         <div className="text-left">
           <h1 className="text-6xl text-slate-700 font-semibold">Taccuino</h1>
         </div>
-        <div id="snippets">
-          <section className="grid grid-cols-1 gap-6 md:grid-cols-2">
+        <div id="snippets" data-testid="page-content">
+          <div className="grid grid-cols-1 gap-6 md:grid-cols-2">
             <MarkdownCard />
             <div className="rounded-lg bg-slate-600 p-6 text-slate-300">
               <h3 className="mb-2 text-xl font-semibold">Snippet 2</h3>
               <p>Snippet content.</p>
             </div>
-          </section>
+          </div>
         </div>
-      </section>
+      </div>
     </main>
   );
 }
