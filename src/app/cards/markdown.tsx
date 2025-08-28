04d1c4707b60187625638a788c8b7a83653b7728
Nausicaa <nlusano89@gmail.com>
2025-08-28 22:16:49 +0200

    Use CardLabels component with fitxure data

--- src/app/cards/markdown.tsx
+++ src/app/cards/markdown.tsx
@@ -1,12 +1,13 @@
 import { GitHubMarkdown } from "@/components/data/markdown";
-import { Card, CardContent, CardHeader, CardTitle } from "@/components/ui/card";
+import { Card, CardContent, CardHeader, CardLabels, CardTitle } from "@/components/ui/card";
 import MarkdownTogglableDetails from "./markdown_togglableDetails";
 
 export default function MarkdownCard() {
-  const { title } = GitHubMarkdown;
+  const { title, labels } = GitHubMarkdown;
   return (
     <Card data-testid="markdown-card" className="p-3.5 shadow-md">
       <CardHeader>
+        <CardLabels labels={labels} />
         <CardTitle className="text-3xl font-semibold">{title}</CardTitle>
       </CardHeader>
       <CardContent data-testid="markdown-card-content" className="rounded-sm">
