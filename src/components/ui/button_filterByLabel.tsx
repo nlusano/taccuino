028eae68782244b24e17dfd9fbe75551ec9d2e84
Nausicaa <nlusano89@gmail.com>
2025-08-30 23:14:39 +0200

    Update FilterByLabelButton component WIP

--- src/components/ui/button_filterByLabel.tsx
+++ src/components/ui/button_filterByLabel.tsx
@@ -7,27 +7,39 @@ import { Button } from "./button";
 function FilterByLabelButton(props: React.ComponentProps<"button"> & {
   label:
   {
-    display: string,
+    display: string
+    isActive?: string
     name: string
   }
 }) {
   const { label } = props
-  const toggleSelected = () => {
-    console.log("click")
-  }
+  const { display, isActive, name } = label
   return (
+    // <Button
+    //   data-testid="filter-button" OK
+    //   name={label.name + "-label"} OK
+    //   size="sm" OK
+    //   className={cn(
+    //     "p-1 pl-1.5 pr-1.5 bg-slate-600 hover:bg-gray-700 text-xs/70 text-slate-200 shadow-slate-700",
+    //     props.className
+    //   )}
+    //   {...props}
+    // >
+    //   {label.display}
+    // </Button>
+
     <Button
       data-testid="filter-button"
-      name={label.name + "-label"}
+      name={name + "-label"}
       size="sm"
       className={cn(
-        "p-1 pl-1.5 pr-1.5 bg-slate-600 hover:bg-gray-700 text-xs/70 text-slate-200 shadow-slate-700",
+        "p-1 pl-1.5 pr-1.5 m-0.5 text-xs/70 text-slate-200 shadow-slate-700",
+        name === isActive ? "hover:bg-rose-950" : "hover:bg-gray-700",
+        name === isActive ? "bg-rose-900" : "bg-slate-600",
         props.className
       )}
-      {...props}
-      onClick={() => toggleSelected()}
     >
-      {label.display}
+      {display}
     </Button>
   )
 }
