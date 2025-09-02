cc9f98425cc9cfaebd2870d7b25909c18c501cc8
Nausicaa <nlusano89@gmail.com>
2025-09-02 19:22:28 +0200

    Add prettier, add configs to avoid conflicts between eslint and prettier rules

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -10,7 +10,12 @@ const compat = new FlatCompat({
 });
 
 const eslintConfig = [
-  ...compat.extends("next/core-web-vitals", "next/typescript"),
+  ...compat.extends(
+    "next/core-web-vitals",
+    "next/typescript",
+    "plugin:@typescript-eslint/recommended",
+    "prettier",
+  ),
 ];
 
 export default eslintConfig;
