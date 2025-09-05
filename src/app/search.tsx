a53e3004a45f022cc41549f3d2c4cb27556399f3
Nausicaa <nlusano89@gmail.com>
2025-09-05 18:56:14 +0200

    Update Search component, add cancel button to reset query

--- src/app/search.tsx
+++ src/app/search.tsx
@@ -1,20 +1,28 @@
 "use client";
-import { SearchIcon } from "lucide-react";
+import { SearchIcon, XIcon } from "lucide-react";
 import { usePathname, useRouter, useSearchParams } from "next/navigation";
+import { useState } from "react";
 
 export default function Search({ placeholder }: { placeholder?: string }) {
   const searchParams = useSearchParams();
   const pathname = usePathname();
   const { replace } = useRouter();
+  const [queryText, setQueryText] = useState("");
 
   function handleSearch(term: string) {
     const params = new URLSearchParams(searchParams);
+
     if (term) {
       params.set("query", term);
     } else {
       params.delete("query");
     }
     replace(`${pathname}?${params.toString()}`);
+  }
+
+  function handleClick() {
+    handleSearch("");
+    setQueryText("");
   }
 
   return (
@@ -27,10 +35,16 @@ export default function Search({ placeholder }: { placeholder?: string }) {
         placeholder={placeholder}
         onChange={(e) => {
           handleSearch(e.target.value);
+          setQueryText(e.target.value);
         }}
-        defaultValue={searchParams.get("query") || ""}
+        // defaultValue={queryText}
+        value={searchParams.get("query") || queryText}
       />
       <SearchIcon className="absolute left-3 top-1/2 h-[18px] w-[18px] -translate-y-1/2 text-gray-500 peer-focus:text-gray-900" />
+      <XIcon
+        className="absolute left-43 top-1/2 h-[18px] w-[18px] -translate-y-1/2 text-gray-500 peer-focus:text-gray-900"
+        onClick={() => handleClick()}
+      />
     </div>
   );
 }
