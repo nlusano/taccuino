596b758168c16c1098aa3a6611b35d28fd8579c1
Nausicaa <nlusano89@gmail.com>
2025-09-06 17:38:16 +0200

    Cleanup

--- src/app/search.tsx
+++ src/app/search.tsx
@@ -37,8 +37,7 @@ export default function Search({ placeholder }: { placeholder?: string }) {
           handleSearch(e.target.value);
           setQueryText(e.target.value);
         }}
-        // defaultValue={queryText}
-        value={searchParams.get("query") || queryText}
+                value={searchParams.get("query") || queryText}
       />
       <SearchIcon className="absolute left-3 top-1/2 h-[18px] w-[18px] -translate-y-1/2 text-gray-500 peer-focus:text-gray-900" />
       <XIcon
