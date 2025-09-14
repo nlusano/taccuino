0a4d907657601a6c082a95b9ebec9cfed19831cf
Nausicaa <nlusano89@gmail.com>
2025-09-14 11:53:50 +0200

    Add intl for consistency and to avoid typos etc.

---
+++ src/components/data/appData.ts
@@ -0,0 +1,15 @@
+const homepage = {
+  title: "Taccuino",
+};
+
+export const cards = {
+  markdown: { title: "Markdown" },
+  sql: { title: "SQL" },
+};
+
+const filter = {
+  markdown: "markdown",
+  sql: "sql",
+};
+
+export const intl = { filter, homepage, cards };
